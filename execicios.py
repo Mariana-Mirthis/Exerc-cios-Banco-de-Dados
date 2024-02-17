c45763f3852d8f65cdc236fd3ac154e317feda6f
Mariana-Mirthis <mirthismariana@gmail.com>
2024-02-17 16:57:29 -0300

    Criar tabela alunos

--- execicios.py
+++ execicios.py
@@ -3,6 +3,9 @@ import sqlite3
 exercicios = sqlite3.connect('banco')
 cursor = exercicios.cursor()
 
+# 1. Crie uma tabela chamada "alunos" com os seguintes campos: id (inteiro), nome (texto), idade (inteiro) e curso (texto).
+cursor.execute('CREATE TABLE alunos(id INT, nome VARCHAR(100), idade INT, curso VARCHAR(100));')
+
 
 
 exercicios.commit()
